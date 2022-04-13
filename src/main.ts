bd71c5d90888a8a3f8cce4f0f1b792d811bbe5d2
Luke Harding <lukeharding990@gmail.com>
2022-04-13 09:02:59 -0400

    Favicon and threejs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
-import './style.css'
+import "./style.css";
+import * as THREE from "three";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+// init
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const camera = new THREE.PerspectiveCamera(
+  70,
+  window.innerWidth / window.innerHeight,
+  0.01,
+  10
+);
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animation);
+document.body.appendChild(renderer.domElement);
+
+// animation
+
+function animation(time: number) {
+  mesh.rotation.x = time / 2000;
+  mesh.rotation.y = time / 1000;
+
+  renderer.render(scene, camera);
+}
+
+window.addEventListener("resize", onWindowResize, false);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
